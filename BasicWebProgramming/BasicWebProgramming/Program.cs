4b0a34ea115ddfd3d05eff28eb75f99403ae8188
stpriyanka <stpriyanka2011@gmail.com>
2016-09-01 16:07:18 +0200

    initial commit. Run server project

---
+++ BasicWebProgramming/BasicWebProgramming/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebProgramming
+{
+
+
+	[ServiceContract]
+	public interface IService
+	{
+		[OperationContract]
+		string Ping(string name);
+	}
+
+
+
+	[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+	internal class ServiceImplementation : IService
+	{
+		#region IService Members
+
+		public string Ping(string name)
+		{
+			Console.WriteLine("SERVER - Processing Ping('{0}')", name);
+			return "Hello, " + name;
+		}
+
+		#endregion
+	}
+
+	public class Program
+	{
+		private static System.Threading.AutoResetEvent stopFlag = new System.Threading.AutoResetEvent(false);
+
+		public static void Main()
+		{
+			ServiceHost svh = new ServiceHost(typeof(ServiceImplementation));
+			svh.AddServiceEndpoint(
+				typeof(IService),
+				new NetTcpBinding(),
+				"net.tcp://localhost:8000");
+			svh.Open();
+
+			Console.WriteLine("SERVER - Running...");
+			stopFlag.WaitOne();
+
+			Console.WriteLine("SERVER - Shutting down...");
+			svh.Close();
+
+			Console.WriteLine("SERVER - Shut down!");
+		}
+
+		public static void Stop()
+		{
+			stopFlag.Set();
+		}
+	
+
+
+	}
+}
+
---
+++ BasicWebProgramming/Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using BasicWebProgramming;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("WCF Simple Demo");
+
+			// start server
+			System.Threading.Thread thServer = new System.Threading.Thread(BasicWebProgramming.Program.Main);
+			thServer.IsBackground = true;
+			thServer.Start();
+			System.Threading.Thread.Sleep(1000);  // wait for server to start up
+
+			// run client
+			ChannelFactory<IService> scf;
+			scf = new ChannelFactory<IService>(
+						new NetTcpBinding(),
+						"net.tcp://localhost:8000");
+
+			IService s;
+			s = scf.CreateChannel();
+
+			while (true)
+			{
+				Console.Write("CLIENT - Name: ");
+				string name = Console.ReadLine();
+				if (name == "")
+					break;
+
+				string response = s.Ping(name);
+				Console.WriteLine("CLIENT - Response from service: " + response);
+			}
+			(s as ICommunicationObject).Close();
+			// shutdown server
+			BasicWebProgramming.Program.Stop();
+			thServer.Join();
+		}
+
+	}
+}
